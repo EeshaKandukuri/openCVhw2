5e27455d346b3fd9b6aa0f493f0732d2af467f7f
Mansi Madhani <mansi@cs.utexas.edu>
2024-05-28 15:55:18 -0500

    updates to code

--- ColorFilter.cpp
+++ ColorFilter.cpp
@@ -14,58 +14,146 @@ void ColorFilter::processImage(cv::Mat img) {
 }
 
 void ColorFilter::split() {
-    _chans = vector<cv::Mat>;
-    split(_frame, _chans);
+    cv::split(_frame, _chans);
 }
 
 void ColorFilter::showResult() {
+
+    //PROBLEM 1
     //imshow("result", _frame);
-    waitKey(1);
+
     //PROBLEM 2: B
     //imshow("blue", _chans[0]);
+
     //PROBLEM 2: G
     //imshow("green", _chans[1]);
+
     //PROBLEM 2: R
     //imshow("red", _chans[2]);
 
     //PROBLEM 3: Blue Subtraction
-    //imshow("blue sub", _chans[0]);
+    //imshow("blue subtraction", _chans[0]);
+
+    //PROBLEM 3: Blue Threshold
+    //imshow("blue threshold", _chans[0]);
+
+    //PROBLEM 3: Largest Blue Blob
+    //imshow("largest blue blob", _chans[0]);
+
+    //PROBLEM 3: Blue Cup
+    //imshow("blue cup", _chans[0]);
+
     //PROBLEM 3: Green Subtraction
-    //imshow("green sub", _chans[1]);
+    //imshow("green subtraction", _chans[1]);
+
+    //PROBLEM 3: Green Threshold
+    //imshow("green threshold", _chans[1]);
+
+    ////PROBLEM 3: Largest Green Blob
+    //imshow("largest green blob", _chans[1]);
+
+    //PROBLEM 3: Green Cup
+    //imshow("green cup", _chans[1]);
+
     //PROBLEM 3: Red Subtraction
-    //imshow("red sub", _chans[2]);
+    //imshow("red subtraction", _chans[2]);
+
+    //PROBLEM 3: Red Threshold
+    //imshow("red threshold", _chans[2]);
+
+    ////PROBLEM 3: Largest Red Blob
+    //imshow("largest red blob", _chans[2]);
+
+    //PROBLEM 3: Red Cup
+    imshow("red cup", _chans[2]);
+
+
+    waitKey(100);
 }
 
 void ColorFilter::findBlue() {
-
     //4.3.1
-    subtract(_chans[0], _chans[2], bMinusR);
+    subtract(_chans[0], _chans[2], _chans[0]);
 
     //4.3.2
-    //SAVE FOR LATER
+    threshold(_chans[0], _chans[0], 50, 255, cv::THRESH_BINARY);
 
     //4.3.3
     vector<cv::Mat> contours;
-    vector<cv:Vec4i> hierarchy;
+    vector<cv::Vec4i> hierarchy;
     findContours(_chans[0], contours, hierarchy, cv::RETR_CCOMP, cv::CHAIN_APPROX_SIMPLE);
     int highest = 0;
+
     for (int i = 0; i < contours.size(); i++) {
         if (contourArea(contours[i]) > contourArea(contours[highest])) {
             highest = i;
         }
     }
-    cv::Mat _zeros = zeros();
-    drawContours(zeros, contours, highest, cv::Scalar(255), cv::LineTypes::FILLED, 8, hierachy);
+    cv::Mat _zeros = cv::Mat::zeros(_frame.rows, _frame.cols, CV_8UC1);
+
+    //display mask
+    drawContours(_zeros, contours, highest, cv::Scalar(255), cv::LineTypes::FILLED, 8, hierarchy);
+    //copyTo(_chans[0], _chans[0], _zeros);
+
+    //display blue cup
+    copyTo(_frame, _chans[0], _zeros);
+     
+    
 }
 
 void ColorFilter::findGreen() {
-    subtract(_chans[1], _chans[2], gMinusR);
+    subtract(_chans[1], _chans[0], _chans[1]);
+
+    threshold(_chans[1], _chans[1], 50, 255, cv::THRESH_BINARY);
+
+    vector<cv::Mat> contours;
+    vector<cv::Vec4i> hierarchy;
+    findContours(_chans[1], contours, hierarchy, cv::RETR_CCOMP, cv::CHAIN_APPROX_SIMPLE);
+    int highest = 0;
+
+    for (int i = 0; i < contours.size(); i++) {
+        if (contourArea(contours[i]) > contourArea(contours[highest])) {
+            highest = i;
+        }
+    }
+    cv::Mat _zeros = cv::Mat::zeros(_frame.rows, _frame.cols, CV_8UC1);
+
+    //display mask
+    drawContours(_zeros, contours, highest, cv::Scalar(255), cv::LineTypes::FILLED, 8, hierarchy);
+    //copyTo(_chans[1], _chans[1], _zeros);
+
+    //display green cup
+    copyTo(_frame, _chans[1], _zeros);
+
 }
 
 void ColorFilter::findRed() {
-    subtract(_chans[2], _chans[0], rMinusB);
+    
+    subtract(_chans[2], _chans[1], _chans[2]);
+
+    threshold(_chans[2], _chans[2], 50, 255, cv::THRESH_BINARY);
+
+    vector<cv::Mat> contours;
+    vector<cv::Vec4i> hierarchy;
+    findContours(_chans[2], contours, hierarchy, cv::RETR_CCOMP, cv::CHAIN_APPROX_SIMPLE);
+    int highest = 0;
+
+    for (int i = 0; i < contours.size(); i++) {
+        if (contourArea(contours[i]) > contourArea(contours[highest])) {
+            highest = i;
+        }
+    }
+    cv::Mat _zeros = cv::Mat::zeros(_frame.rows, _frame.cols, CV_8UC1);
+
+    //display mask
+    drawContours(_zeros, contours, highest, cv::Scalar(255), cv::LineTypes::FILLED, 8, hierarchy);
+    copyTo(_chans[2], _chans[2], _zeros);
+
+    //display red cup
+    //copyTo(_frame, _chans[2], _zeros);
 }
 
 void ColorFilter::findBGR() {
+
 
 }
---
+++ build/CMakeFiles/cvexample.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/u/mansi/openCVhw2/ColorFilter.cpp" "/u/mansi/openCVhw2/build/CMakeFiles/cvexample.dir/ColorFilter.cpp.o"
+  "/u/mansi/openCVhw2/ReadAVI.cpp" "/u/mansi/openCVhw2/build/CMakeFiles/cvexample.dir/ReadAVI.cpp.o"
+  "/u/mansi/openCVhw2/cvexample.cpp" "/u/mansi/openCVhw2/build/CMakeFiles/cvexample.dir/cvexample.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
